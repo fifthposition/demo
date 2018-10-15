637602c588f516de2ecd0e04ce3d36b85957b533
Ben Klein <bklein@pivotal.io>
2018-10-15 11:47:22 -0700

    Add comment

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -3,6 +3,7 @@ package com.example.demo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+//A Comment
 @SpringBootApplication
 public class DemoApplication {
 
